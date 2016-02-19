a84bf5beaf3b6c09338d3f6224c5d1ef98077b9f
Cake <kimquiros@live.com>
2016-02-18 18:45:36 -0600

    portafolio

---
+++ components/js/script.js
@@ -0,0 +1,283 @@
+//NAV ONEPAGE
+$(document).ready(function() {
+  $('#nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750
+  });
+});
+
+$.fn.extend({
+  
+  // Define the threeBarToggle function by extending the jQuery object
+  threeBarToggle: function(options){
+    
+    // Set the default options
+    var defaults = {
+      color: 'black',
+      width: 30,
+      height: 25,
+      speed: 400,
+      animate: true
+    }
+    var options = $.extend(defaults, options); 
+    
+    return this.each(function(){
+      
+      $(this).empty().css({'width': options.width, 'height': options.height, 'background': 'transparent'});
+      $(this).addClass('tb-menu-toggle');
+      $(this).prepend('<i></i><i></i><i></i>').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('tb-active-toggle');
+        if (options.animate) { $(this).toggleClass('tb-animate-toggle'); }
+        $('.tb-mobile-menu').slideToggle(options.speed);
+      });
+      $(this).children().css('background', options.color);
+    });
+  },
+  
+  // Define the accordionMenu() function that adds the sliding functionality
+  accordionMenu: function(options){
+    
+    // Set the default options
+    var defaults = {
+      speed: 400
+    }
+    var options =  $.extend(defaults, options);
+
+    return this.each(function(){
+      
+      $(this).addClass('tb-mobile-menu');
+      var menuItems = $(this).children('li');
+      menuItems.find('.sub-menu').parent().addClass('tb-parent');
+      $('.tb-parent ul').hide();
+      $('.tb-parent > a').on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $(this).siblings().slideToggle(options.speed);
+      });
+      
+    });
+  }
+});
+
+// Convert any element into a three bar toggle
+// Optional arguments are 'speed' (number in ms, 'slow' or 'fast') and 'animation' (true or false) to disable the animation on the toggle
+$('#menu-toggle').threeBarToggle({color: '#fff', width: 40, height: 30});
+
+// Make any nested ul-based menu mobile
+// Optional arguments are 'speed' and 'accordion' (true or false) to disable the behavior of closing other sub
+$('#menu').accordionMenu();
+
+
+//CODE CANVAS
+/*
+ * requestAnimationFrame pollyfill
+ */
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
+        return window.setTimeout(callback, 1000 / 60);
+    });
+}
+
+// Init Stats
+var stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+
+
+/*!
+ * Mantis.js / jQuery / Zepto.js plugin for Constellation
+ * @version 1.2.2
+ * @author Acauã Montiel <contato@acauamontiel.com.br>
+ * @license http://acaua.mit-license.org/
+ */
+(function ($, window) {
+    /**
+     * Makes a nice constellation on canvas
+     * @constructor Constellation
+     */
+    function Constellation (canvas, options) {
+        var $canvas = $(canvas),
+            context = canvas.getContext('2d'),
+            defaults = {
+                star: {
+                    color: '#AE324D',
+                    width: 1
+                },
+                line: {
+                    color: '#AE324D',
+                    width: 0.2
+                },
+                position: {
+                    x: 0, // This value will be overwritten at startup
+                    y: 0 // This value will be overwritten at startup
+                },
+                width: window.innerWidth,
+                height: window.innerHeight,
+                velocity: 0.1,
+                length: 100,
+                distance: 120,
+                radius: 150,
+                stars: []
+            },
+            config = $.extend(true, {}, defaults, options);
+
+        function Star () {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+
+            this.vx = (config.velocity - (Math.random() * 0.5));
+            this.vy = (config.velocity - (Math.random() * 0.5));
+
+            this.radius = Math.random() * config.star.width;
+        }
+
+        Star.prototype = {
+            create: function(){
+                context.beginPath();
+                context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                context.fill();
+            },
+
+            animate: function(){
+                var i;
+                for (i = 0; i < config.length; i++) {
+
+                    var star = config.stars[i];
+
+                    if (star.y < 0 || star.y > canvas.height) {
+                        star.vx = star.vx;
+                        star.vy = - star.vy;
+                    } else if (star.x < 0 || star.x > canvas.width) {
+                        star.vx = - star.vx;
+                        star.vy = star.vy;
+                    }
+
+                    star.x += star.vx;
+                    star.y += star.vy;
+                }
+            },
+
+            line: function(){
+                var length = config.length,
+                    iStar,
+                    jStar,
+                    i,
+                    j;
+
+                for (i = 0; i < length; i++) {
+                    for (j = 0; j < length; j++) {
+                        iStar = config.stars[i];
+                        jStar = config.stars[j];
+
+                        if (
+                            (iStar.x - jStar.x) < config.distance &&
+                            (iStar.y - jStar.y) < config.distance &&
+                            (iStar.x - jStar.x) > - config.distance &&
+                            (iStar.y - jStar.y) > - config.distance
+                        ) {
+                            if (
+                                (iStar.x - config.position.x) < config.radius &&
+                                (iStar.y - config.position.y) < config.radius &&
+                                (iStar.x - config.position.x) > - config.radius &&
+                                (iStar.y - config.position.y) > - config.radius
+                            ) {
+                                context.beginPath();
+                                context.moveTo(iStar.x, iStar.y);
+                                context.lineTo(jStar.x, jStar.y);
+                                context.stroke();
+                                context.closePath();
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        this.createStars = function () {
+            var length = config.length,
+                star,
+                i;
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (i = 0; i < length; i++) {
+                config.stars.push(new Star());
+                star = config.stars[i];
+
+                star.create();
+            }
+
+            star.line();
+            star.animate();
+        };
+
+        this.setCanvas = function () {
+            canvas.width = config.width;
+            canvas.height = config.height;
+        };
+
+        this.setContext = function () {
+            context.fillStyle = config.star.color;
+            context.strokeStyle = config.line.color;
+            context.lineWidth = config.line.width;
+        };
+
+        this.setInitialPosition = function () {
+            if (!options || !options.hasOwnProperty('position')) {
+                config.position = {
+                    x: canvas.width * 0.5,
+                    y: canvas.height * 0.5
+                };
+            }
+        };
+
+        this.loop = function (callback) {
+            callback();
+
+            window.requestAnimationFrame(function () {
+                stats.begin(); // Only for Stats
+                this.loop(callback);
+                stats.end(); // Only for Stats
+            }.bind(this));
+        };
+
+        this.bind = function () {
+            $canvas.on('mousemove', function(e){
+                config.position.x = e.pageX - $canvas.offset().left;
+                config.position.y = e.pageY - $canvas.offset().top;
+            });
+        };
+
+        this.init = function () {
+            this.setCanvas();
+            this.setContext();
+            this.setInitialPosition();
+            this.loop(this.createStars);
+            this.bind();
+        };
+    }
+
+    $.fn.constellation = function (options) {
+        return this.each(function () {
+            var c = new Constellation(this, options);
+            c.init();
+        });
+    };
+})($, window);
+
+// Init plugin
+$('canvas').constellation({
+    star: {
+        width: 3
+    },
+    line: {
+        color: 'rgba(255, 255, 255, .5)'
+    },
+    radius: 250
+});
+
